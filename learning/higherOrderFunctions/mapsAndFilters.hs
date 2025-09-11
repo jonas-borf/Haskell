ad73c0846bb2c6854a03dd0b0925e6929516c3d5
jonas-borf <107635451+jonas-borf@users.noreply.github.com>
2025-09-11 08:22:02 -0400

    09-11-2025-8:22

--- learning/higherOrderFunctions/mapsAndFilters.hs
+++ learning/higherOrderFunctions/mapsAndFilters.hs
@@ -24,3 +24,14 @@ largestDivisible = head (filter' p [100000,99999..])
 
 --Sum of all odd squares that are smaller than 10,000
 --sum (takeWhile (<10000) (filter' odd (map (^2) [1..])))
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n 
+    | even n = n:chain (div n 2)
+    | odd n = n:chain (n*3+1)
+
+numLongChains :: Int
+numLongChains = length (filter' isLong (map' chain [1..100]))
+    where isLong xs = length xs > 15
+
