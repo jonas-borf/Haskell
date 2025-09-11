7a29eeb6c40d639b16c6a01cf6eb300cc32990b1
jonas-borf <107635451+jonas-borf@users.noreply.github.com>
2025-09-11 08:07:03 -0400

    09-11-2025-8:06

---
+++ learning/higherOrderFunctions/mapsAndFilters.hs
@@ -0,0 +1,26 @@
+map' :: (a->b)->[a]->[b]
+map' _ [] = []
+map' f (x:xs) = f x:map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x = x : filter' p xs
+    | otherwise = filter' p xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort (filter' (<=x) xs)
+        biggerSorted = quicksort (filter' (>x) xs)
+    in smallerSorted ++ [x] ++ biggerSorted
+
+--Find the largest number under 100,000 that is divisible
+--by 3829.
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter' p [100000,99999..])
+    where p x = mod x 3829 == 0
+
+--Sum of all odd squares that are smaller than 10,000
+--sum (takeWhile (<10000) (filter' odd (map (^2) [1..])))
