b51e6f1c3fd08268a85df14f3be37d4830df5021
jonas-borf <107635451+jonas-borf@users.noreply.github.com>
2025-09-12 08:06:57 -0400

    Fri Sep 12 08:06:57 EDT 2025

--- learning/higherOrderFunctions/onlyFoldsAndHorses.hs
+++ learning/higherOrderFunctions/onlyFoldsAndHorses.hs
@@ -3,3 +3,30 @@ sum' xs = foldl (\acc x -> acc + x) 0 xs
 
 sum'' :: (Num a) => [a] -> a
 sum'' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+map' :: (a->b) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
+
+maximum' :: (Ord a) => [a] -> a
+maximum'= foldr1 (\x acc -> if x > acc then x else acc) 
+
+reverse' :: [a] -> [a]
+reverse'= foldl (\acc x -> x : acc) []
+
+product' :: (Num a) => [a] -> a
+product' = foldr1 (*)
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+head' :: [a] -> a
+head' = foldr1 (\x _ -> x)
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
