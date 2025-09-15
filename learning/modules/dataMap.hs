85f1592459486e5e17106d7add782d7af901d8eb
jonas-borf <107635451+jonas-borf@users.noreply.github.com>
2025-09-15 08:47:08 -0400

    Mon Sep 15 08:47:08 EDT 2025

--- learning/modules/dataMap.hs
+++ learning/modules/dataMap.hs
@@ -1,3 +1,35 @@
-import Data.Map
+import qualified Data.Map as Map
 
-findKey :: (Eq k) => k -> [(k,v )]
+phoneBook = 
+    [("betty","555-2938")
+    ,("betty","342-2492")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("patsy","943-2929")
+    ,("patsy","827-9162")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ,("penny","555-2111")
+    ]
+
+{-
+findKey :: (Eq k) => k -> [(k,v )] -> v
+findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+-}
+{-
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key [] = Nothing
+findKey key ((k,v):xs) = if key == k
+                            then Just v
+                            else findKey key xs
+-}
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing 
+
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith (\number1 number2 -> number1 ++ ", " ++ number2) xs 
