3816e6e1b13e148ca31d5015e408575f5ec12cbd
jonas-borf <107635451+jonas-borf@users.noreply.github.com>
2025-09-20 17:13:36 -0400

    Sat Sep 20 17:13:36 EDT 2025

--- learning/inputAndOutput/filesAndStreams.hs
+++ learning/inputAndOutput/filesAndStreams.hs
@@ -1,5 +1,10 @@
-import Data.Char
-
 main = do
   contents <- getContents
-  putStr (map toUpper contents)
+  putStr (shortLinesOnly contents)
+
+shortLinesOnly :: String -> String
+shortLinesOnly input =
+  let allLines = lines input
+      shortLines = filter (\line -> length line < 10) allLines
+      result = unlines shortLines
+   in result
