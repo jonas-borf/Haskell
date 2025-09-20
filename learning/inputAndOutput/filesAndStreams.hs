d2ccae9af05503c5aaa65e5144093c31beffb1f3
jonas-borf <107635451+jonas-borf@users.noreply.github.com>
2025-09-20 17:25:54 -0400

    Sat Sep 20 17:25:54 EDT 2025

--- learning/inputAndOutput/filesAndStreams.hs
+++ learning/inputAndOutput/filesAndStreams.hs
@@ -1,2 +1,6 @@
-main = interact $ unlines . filter ((<10) . length) . lines
+main = interact respondPalindromes
 --This is a possible solution, but much more difficult to read.
+
+respondPalindromes = unlines . map (\xs -> if isPalindrome xs then "palindrome" else "not a palindrome") . lines
+  where
+    isPalindrome xs = xs == reverse xs
