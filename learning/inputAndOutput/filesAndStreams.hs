5da477d742b27078c74c25370834a7e4e39fd254
jonas-borf <107635451+jonas-borf@users.noreply.github.com>
2025-09-20 17:17:29 -0400

    Sat Sep 20 17:17:29 EDT 2025

--- learning/inputAndOutput/filesAndStreams.hs
+++ learning/inputAndOutput/filesAndStreams.hs
@@ -1,8 +1,2 @@
-main = interact shortLinesOnly
-
-shortLinesOnly :: String -> String
-shortLinesOnly input =
-  let allLines = lines input
-      shortLines = filter (\line -> length line < 10) allLines
-      result = unlines shortLines
-   in result
+main = interact $ unlines . filter ((<10) . length) . lines
+--This is a possible solution, but much more difficult to read.
