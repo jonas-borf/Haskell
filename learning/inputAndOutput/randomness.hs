87112e0a46302b77c6ef7e980c1c017b6f8cd826
jonas-borf <107635451+jonas-borf@users.noreply.github.com>
2025-09-21 00:42:44 -0400

    Sun Sep 21 00:42:44 EDT 2025

---
+++ learning/inputAndOutput/randomness.hs
@@ -0,0 +1,22 @@
+import System.Random
+
+main = do
+    gen <- getStdGen
+    putStr $ take 20 (randomRs ('a', 'z') gen)
+    
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+    let (firstCoin, newGen) = random gen
+        (secondCoin, newGen') = random newGen
+        (thirdCoin, newGen'') = random newGen'
+    in (firstCoin, secondCoin, thirdCoin)
+
+randoms' :: (RandomGen g, Random a) => g -> [a]
+randoms' gen = let (value, newGen) = random gen in value:randoms' newGen
+
+finiteRandoms :: (RandomGen g, Random a, Integral n) => n -> g -> ([a], g)
+finiteRandoms 0 gen = ([], gen)
+finiteRandoms n gen =
+    let (value, newGen) = random gen
+        (restOfList, finalGen) = finiteRandoms (n-1) newGen
+    in (value:restOfList, finalGen)
