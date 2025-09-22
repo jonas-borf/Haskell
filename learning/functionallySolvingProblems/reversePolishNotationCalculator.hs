a08457bc644a24b66346da45fe3325e90076109a
jonas-borf <jonasbarfield@gmail.com>
2025-09-22 05:23:08 -0400

    Mon Sep 22 05:23:08 AM EDT 2025

---
+++ learning/functionallySolvingProblems/reversePolishNotationCalculator.hs
@@ -0,0 +1,15 @@
+import Data.List
+
+solveRPN :: String -> Float
+solveRPN = head . foldl foldingFunction [] . words
+	where	foldingFunction (x:y:ys) "*" = (x*y):ys
+		foldingFunction (x:y:ys) "+" = (x+y):ys
+		foldingFunction (x:y:ys) "-" = (y-x):ys
+		foldingFunction (x:y:ys) "/" = (y/x):ys
+		foldingFunction (x:y:ys) "^" = (y**x):xs
+		--etc
+		
+		foldingFunction xs numberString = read numberString:xs
+	
+	
+		
